7031ed9128df679ebf961b7e3536297e16eb8616
chloechengr <chengsn@bc.edu>
2019-08-26 19:42:14 -0400

    initial

---
+++ Sample/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Sample
+//
+//  Created by Chloe Cheng on 8/26/19.
+//  Copyright © 2019 Chloe Cheng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("View did load")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
